9dba6c225808eaea18e7457a8db8256c124d9c91
Goos <goos.bekerom@gmail.com>
2018-04-02 10:11:56 +0200

    initial commit

---
+++ src/main/kotlin/com/goosvandenbekerom/roulette/dealer/DealerApplication.kt
@@ -0,0 +1,11 @@
+package com.goosvandenbekerom.roulette.dealer
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DealerApplication : CommandLineRunner {
+    override fun run(vararg args: String?) {
+
+    }
+}
