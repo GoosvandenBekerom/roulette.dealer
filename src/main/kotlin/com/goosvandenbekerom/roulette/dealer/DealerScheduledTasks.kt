39b6898a7c4b5338c8604f62b2681d6b8804db1e
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-24 00:37:11 +0200

    Added task to update players on round time

--- src/main/kotlin/com/goosvandenbekerom/roulette/dealer/DealerScheduledTasks.kt
+++ src/main/kotlin/com/goosvandenbekerom/roulette/dealer/DealerScheduledTasks.kt
@@ -13,17 +13,27 @@ import java.util.*
 
 @Component
 class DealerScheduledTasks : CommandLineRunner {
+    companion object {
+        private const val spinInterval: Long = 30000
+        private const val updateInterval: Long = 5000
+    }
+
     @Autowired lateinit var game: Game
     @Autowired lateinit var rabbit: RabbitTemplate
     @Autowired lateinit var updateExchange: FanoutExchange
 
+    private var roundStartedTime = Date()
+
     override fun run(vararg args: String?) {
+        // Open betting for the game when the application starts
         game.openBetting()
     }
 
-    @Scheduled(fixedRate = 30000)
+    @Scheduled(fixedRate = spinInterval, initialDelay = spinInterval)
     fun playGameIfReady() {
-        if (game.bets.isEmpty()) return
+        roundStartedTime = Date()
+
+        if (!gameIsReady()) return
 
         game.closeBetting()
         println("Broadcasting 'betting closed' update to connected players")
@@ -40,7 +50,6 @@ class DealerScheduledTasks : CommandLineRunner {
         rabbit.convertAndSend(updateExchange.name, RabbitConfig.UPDATE_PLAYER_ROUTING_KEY, resultUpdate.build())
 
         // TODO: send updated chip amount to each connected player
-        // TODO: test this funtion!!!
 
         game.openBetting()
         println("Broadcasting 'betting opened' update to connected players")
@@ -48,4 +57,15 @@ class DealerScheduledTasks : CommandLineRunner {
         closeBettingUpdate.status = true
         rabbit.convertAndSend(updateExchange.name, RabbitConfig.UPDATE_PLAYER_ROUTING_KEY, openBettingUpdate.build())
     }
+
+//    @Scheduled(fixedRate = updateInterval)
+//    fun updateClients() {
+//        if (roundStartedTime.time < Date().time - 25000) return // The round has more then 5 seconds left
+//        //TODO: if (!gameIsReady()) return
+//
+//        println("The current round will end in less then 5 seconds")
+//        // TODO: send update to clients
+//    }
+
+    private fun gameIsReady(): Boolean = !game.bets.isEmpty()
 }
